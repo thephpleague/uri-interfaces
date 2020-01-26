0668452b447c6e56a0bebb69410083c16730e431
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2020-01-26 20:45:34 +0100

    Improve UriTemplateInterface wording

--- src/Contracts/UriTemplateInterface.php
+++ src/Contracts/UriTemplateInterface.php
@@ -37,6 +37,8 @@ interface UriTemplateInterface
     /**
      * The default value used to expand the template.
      *
+     * The returned list only contains variables whose name is part of the current template.
+     *
      * @return array<string,string|array>
      */
     public function getDefaultVariables(): array;
@@ -50,6 +52,11 @@ interface UriTemplateInterface
     public function expand(array $variables = []): UriInterface;
 
     /**
+     * Returns a new instance with the updated template
+     *
+     * This method MUST retain the state of the current instance, and return
+     * an instance that contains the modified template value.
+     *
      * @param object|string $template a string or an object with the __toString method
      *
      * @throws UriTemplateException if the expansion can not be done.
@@ -57,6 +64,14 @@ interface UriTemplateInterface
     public function withTemplate($template): self;
 
     /**
+     * Returns a new instance with the updated default variables
+     *
+     * This method MUST retain the state of the current instance, and return
+     * an instance that contains the modified default variables.
+     *
+     * If present, variables whose name is not part of the current template
+     * possible variable names are removed.
+     *
      * @param array<string,string|array> $defaultVariables
      */
     public function withDefaultVariables(array $defaultVariables): self;
