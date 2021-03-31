1c3a943b1ea1988f45e993e4022881a40ecccafc
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2021-03-31 10:37:14 +0200

    Adding toString method to Interface

--- src/Contracts/UriComponentInterface.php
+++ src/Contracts/UriComponentInterface.php
@@ -41,6 +41,17 @@ interface UriComponentInterface extends \JsonSerializable
      *
      * If the instance is not defined an empty string is returned
      */
+    public function toString(): string;
+
+    /**
+     * Returns the instance string representation.
+     *
+     * If the instance is defined, the value returned MUST be percent-encoded,
+     * but MUST NOT double-encode any characters. To determine what characters
+     * to encode, please refer to RFC 3986, Sections 2 and 3.
+     *
+     * If the instance is not defined an empty string is returned
+     */
     public function __toString(): string;
 
     /**
