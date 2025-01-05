58ccecf9a0d98d557666c071ea69b735135e15d7
ignace nyamagana butera <nyamsprod@gmail.com>
2025-01-05 21:08:43 +0100

    Uri::getUsername is an alias for Uri::getUser

--- Idna/Converter.php
+++ Idna/Converter.php
@@ -21,6 +21,7 @@ use Stringable;
 use function idn_to_ascii;
 use function idn_to_utf8;
 use function rawurldecode;
+use function strtolower;
 
 use const INTL_IDNA_VARIANT_UTS46;
 
@@ -155,7 +156,7 @@ final class Converter
         idn_to_utf8($domain, $flags->toBytes(), INTL_IDNA_VARIANT_UTS46, $idnaInfo);
 
         if ([] === $idnaInfo) {
-            return Result::fromIntl(['result' => $domain, 'isTransitionalDifferent' => false, 'errors' => Error::NONE->value]);
+            return Result::fromIntl(['result' => strtolower($domain), 'isTransitionalDifferent' => false, 'errors' => Error::NONE->value]);
         }
 
         return Result::fromIntl($idnaInfo);
