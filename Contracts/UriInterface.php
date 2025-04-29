4808a05856a6ab70ec76b632e09267787169d39f
ignace nyamagana butera <nyamsprod@gmail.com>
2025-04-29 12:19:06 +0200

    Remove UriInterface::getUser which was never released

--- Contracts/UriInterface.php
+++ Contracts/UriInterface.php
@@ -22,8 +22,7 @@ use Stringable;
 /**
  * @phpstan-import-type ComponentMap from UriString
  *
- * @method string|null getUsername() returns the user component of the URI (deprecated).
- * @method string|null getUser() returns the user component of the URI.
+ * @method string|null getUsername() returns the user component of the URI.
  * @method self withUser(?string $user) returns a new URI instance with user component updated, if the user is set to null the password also will be set to null.
  * @method string|null getPassword() returns the scheme-specific information about how to gain authorization to access the resource.
  * @method self withPassword(?string $password) returns a new URI instance with password component updated, if the user is set to null the password also will be set to null.
