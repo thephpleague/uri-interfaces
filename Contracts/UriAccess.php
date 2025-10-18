e3a7b84dbfe71b99f687a4c03ff57710944c834c
ignace nyamagana butera <nyamsprod@gmail.com>
2025-10-18 13:30:31 +0200

    Deprecated UriAccess interface

--- Contracts/UriAccess.php
+++ Contracts/UriAccess.php
@@ -15,14 +15,15 @@ namespace League\Uri\Contracts;
 
 use Psr\Http\Message\UriInterface as Psr7UriInterface;
 
-/**
- * @deprecated since version 7.6.0
- */
 interface UriAccess
 {
+    /**
+     * @deprecated since version 7.6.0
+     */
     public function getUri(): UriInterface|Psr7UriInterface;
 
     /**
+     * @deprecated since version 7.6.0
      * Returns the RFC3986 string representation of the complete URI.
      */
     public function getUriString(): string;
